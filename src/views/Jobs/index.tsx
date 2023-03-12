d7d98d97ce5bd5f474b9690f885de9ab65fb0eb5
Edixon Piña <edixonalbertto@gmail.com>
2023-03-11 23:59:50 -0400

    change template in jobs, improve styles
    - add new colors, sizes in global.css
    - add new filter in logic of component jobs
    - show more data in component jobs
    - add new icon in component icons

--- src/views/Jobs/index.tsx
+++ src/views/Jobs/index.tsx
@@ -15,9 +15,14 @@ export function Jobs() {
   useEffect(() => {
     if (jobs.length || search) {
       const filter = jobs.filter(job => {
-        return job.title.toLowerCase().includes(search.toLowerCase())
+        const textInput = search.trim().toLowerCase()
+        // prettier-ignore
+        return (
+          false
+          || job.title.toLowerCase().includes(textInput)
+          || job.details.skills.map(skill => skill.toLowerCase()).includes(textInput)
+        )
       })
-      console.log(filter.length)
       setJobsFilter(filter)
     }
   }, [jobs, search])
@@ -34,7 +39,7 @@ export function Jobs() {
         // prettier-ignore
         return (
           true
-          // && job.details.postulations < 100
+          && job.details.postulations < 50
           && job.details.remote100
           && job.details.language === 'spanish'
         )
@@ -57,43 +62,63 @@ export function Jobs() {
         {jobsFilter.length ? (
           jobsFilter.map((job: TJob, i: number) => (
             <li key={i} className="jobs__container__card">
-              <div className="jobs__card__title">
-                <h3>{job.title}</h3>
-                {job.isNew && <span>Nuevo</span>}
+              <div className="card__section jobs__card__content">
+                <div className="jobs__content__text">
+                  <div className="jobs__content__item">
+                    <h3>{job.title}</h3>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Company />
+                    <span>{job.companyName}</span>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Location />
+                    <span>{job.location}</span>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Code />
+                    <span>{job.role}</span>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Clock />
+                    <span>{job.time}</span>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Lang />
+                    <span>{job.details.language === 'spanish' ? 'Español' : job.details.language}</span>
+                  </div>
+
+                  <div className="jobs__content__item">
+                    <Icons.Users />
+                    <span>
+                      {job.details.postulations + (job.details.postulations > 1 ? ' Postulaciones' : ' Postulante')}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="jobs__content__badges">
+                  {job.isNew && <div className="badge__new">Nuevo</div>}
+                  {job.postulationFast && <div className="badge__fast">Rapido</div>}
+                  {job.hasPublishedSalary && <div className="badge__money">Salario</div>}
+                </div>
               </div>
 
-              <div className="jobs__card__content">
-                <div className="jobs__content__item">
-                  <Icons.Company />
-                  <span>{job.companyName}</span>
-                </div>
-
-                <div className="jobs__content__item">
-                  <Icons.Location />
-                  <span>{job.location}</span>
-                </div>
-
-                <div className="jobs__content__item">
-                  <Icons.Code />
-                  <span>{job.role}</span>
-                </div>
-
-                <div className="jobs__content__item">
-                  <Icons.Lang />
-                  <span>{job.details.language === 'spanish' ? 'Español' : job.details.language}</span>
-                </div>
-
-                <div className="jobs__content__item">
-                  <Icons.Users />
-                  <span>
-                    {job.details.postulations + (job.details.postulations > 1 ? ' Postulaciones' : ' Postulante')}
-                  </span>
-                </div>
-
-                <a className="jd__link" href={job.url} target="_blank" rel="noopener noreferrer">
-                  Ver Detalles
-                </a>
+              <div className="card__section jobs__card__badges">
+                {job.details.skills.map(skill => (
+                  <div key={skill} className="jobs__badges__item">
+                    <span>{skill}</span>
+                  </div>
+                ))}
               </div>
+
+              <a className="card__section jd__link" href={job.url} target="_blank" rel="noopener noreferrer">
+                Ver Detalles
+              </a>
             </li>
           ))
         ) : (
